4e82e2dfdfce9dbebd3dedaac7b4a2f4d744d8f8
rio7700 <riolog1234@gmail.com>
2025-01-16 05:18:03 +0000

    buttonの追加

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ function ClickCounterGame(){
 const gameContainer=document.getElementById("game-container");
 
 let button1=document.createElement("button");
+let button2=document.createElement("button");
+let button3=document.createElement("button");
 button1.textContent="+1ボタン";
-
+button2.textContent="+10ボタン";
+button3.textContent="reset";
 let counter=document.createElement("p");
 counter.textContent=count;
 
@@ -12,11 +15,28 @@ button1.addEventListener("click",function(){
     count++;
     counter.textContent=count;
 
+})
+
+button2.addEventListener("click",function(){
+    count = count + 10
+    counter.textContent=count;
+
+})
+
+button3.addEventListener("click",function(){
+    count = count * 0//count = 0;が本当
+    counter.textContent=count;
+
+})
+counter.addEventListener("click",function(){
+    count = 0;
+    counter.textContent=count;
 
 })
 
 gameContainer.appendChild(button1);
+gameContainer.appendChild(button2);
+gameContainer.appendChild(button3);
 gameContainer.appendChild(counter);
-
 }
 ClickCounterGame();
